17b077b56a336cf334b790fdbb89a78efbfca18c
Om Bhatnagar <omishb2001@gmail.com>
2022-11-11 04:03:35 +0530

    Removed React.StrictMode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
@@ -9,12 +10,9 @@ import { Provider } from "react-redux";
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    ,
-  </React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
